645a9d9c937361ed2bd97fce6a2ff5a9cc9373b4
AdaptiveAiAlliance <n.g.majd@gmail.com>
2024-07-28 14:55:04 -0700

    add mdx pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
-export const config = {
-  isProd: true,
-  basePath: "/portfolio.github.io",
-};
+import remarkGfm from "remark-gfm";
+import createMDX from "@next/mdx";
+import { config } from "./src/configs/config.mjs";
+
 const nextConfig = {
   basePath: config.isProd ? config.basePath : "",
   output: "export",
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
 
-export default nextConfig;
+// export default nextConfig;
+export default withMDX(nextConfig);
